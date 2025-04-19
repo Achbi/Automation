55d8366f201b3667681eaa9b072213af7f4afd9b
Achbi <harshitbahety41@gmail.com>
2025-04-19 22:00:33 +0530

    Navbar done

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,16 @@
+import Navba from "./components/global/navbar";
+
+export default function Home(){
+  return(
+    <main> 
+      
+     <div className="color-white">
+      hi
+
+     </div>
+     <Navba>
+      
+     </Navba>
+    </main>
+  )
+}
