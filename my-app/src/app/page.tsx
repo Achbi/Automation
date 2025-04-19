3ea6aa66ac3d07ede9a8905c2664d0c32103e405
Achbi <harshitbahety41@gmail.com>
2025-04-19 22:42:08 +0530

    layout

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,15 +2,10 @@ import Navba from "./components/global/navbar";
 
 export default function Home(){
   return(
-    <main> 
-      
-     <div className="color-white">
-      hi
 
-     </div>
-     <Navba>
-      
-     </Navba>
+     <main className="flex items-center justify-center flex-col">
+      <Navba />
+      <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased"></section>
     </main>
   )
 }
