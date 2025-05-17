087501203faa3c84ee802e5e4a68fa1258273386
Achbi <harshitbahety41@gmail.com>
2025-05-17 19:23:36 +0530

    redux fix

--- my-app/src/app/(main)/(pages)/workflows/_components/workflowbutton.tsx
+++ my-app/src/app/(main)/(pages)/workflows/_components/workflowbutton.tsx
@@ -5,20 +5,11 @@ import { Plus } from 'lucide-react'
 type props = {};
 
 
-  const handleClick = () => {
-    setOpen(
-      <CustomModal
-        title="Create a Workflow Automation"
-        subheading="Workflows are a powerfull that help you automate tasks."
-      >
-        <Workflowform />
-      </CustomModal>
-    )
-  }
+ 
 const WorkflowButton = (props: props) => {
     return(
         <Button size ={'icon'}
-        onClick={handleclick}
+        onClick={handleClick}
         >
             <Plus/>
 
