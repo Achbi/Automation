c17f6414a297b6b0961b4eb566f2b87443766f51
Achbi <harshitbahety41@gmail.com>
2025-06-18 08:01:43 +0530

    upgrade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,8 @@ export default function Home() {
   //WIP: remove fault IMAge for home page
   return (
     <main className="flex items-center justify-center flex-col">
+      <Navbar/>
+
       <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
         <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
         <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
